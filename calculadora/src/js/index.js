761c0a78b7a3c37e9ef40d6435e3bd2ac3e5470c
KayckRossi <Kayck.12gabriel@gmail.com>
2023-03-12 00:38:17 -0300

    coloquei box-shadow nos buttons/cor no placeholder

---
+++ calculadora/src/js/index.js
@@ -0,0 +1,9 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const input = document.getElementById('input1');
+  
+//     input.addEventListener('input', (event) => {
+//       input.style.color = 'red';
+//     });
+// //   }); 
+// codigo para alterar a cor do texto dentro do input
+  

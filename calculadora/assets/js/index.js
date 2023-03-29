29ade527ab6b42a51efcb291b577c0975974c819
KayckRossi <Kayck.12gabriel@gmail.com>
2023-03-29 13:30:10 -0300

    iniciando implementação dos Scripts JS

--- calculadora/assets/js/index.js
+++ calculadora/assets/js/index.js
@@ -7,30 +7,22 @@
 // //   }); 
 // codigo para alterar a cor do texto dentro do input
   
-// obtém os elementos da calculadora
-const input = document.getElementById('input1');
-const btns = document.querySelectorAll('#btns-grups button');
+const keysNum = [...document.querySelectorAll('.num')]
 
-// adiciona eventos de clique aos botões
-btns.forEach(btn => {
-  btn.addEventListener('click', () => {
-    const value = btn.textContent;
-    if (value === 'del') {
-      // remove o último caractere do input
-      input.value = input.value.slice(0, -1);
-    } else if (value === 'AC') {
-      // limpa o input
-      input.value = '';
-    } else if (value === '=') {
-      // calcula a expressão no input
-      try {
-        input.value = eval(input.value);
-      } catch (error) {
-        input.value = 'Erro';
-      }
-    } else {
-      // adiciona o valor do botão ao input
-      input.value += value;
-    }
-  });
-});
+const keysOp = [...document.querySelectorAll('.op')]
+
+const keyResults = document.querySelector('.res')
+
+const display = document.querySelector('.display-p')
+
+keysNum.forEach((el)=>{
+  el.addEventListener("click",(evt)=>{
+    display.innerHTML+=evt.target.innerHTML
+  })
+})
+
+keysOp.forEach((el)=>{
+  el.addEventListener("click",(evt)=>{
+    display.innerHTML+=evt.target.innerHTML
+  })
+})
